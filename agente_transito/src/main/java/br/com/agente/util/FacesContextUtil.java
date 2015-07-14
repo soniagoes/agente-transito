5f8d39f11448494d6558bb81ac5adc9f642d1ea6
36694320191 <36694320191@A5086.detran.ms.gov.br>
2015-07-14 10:37:41 -0300

    adição da entidade PESSOA

--- agente_transito/src/main/java/br/com/agente/util/FacesContextUtil.java
+++ agente_transito/src/main/java/br/com/agente/util/FacesContextUtil.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.agente.util;
 
 import javax.faces.context.FacesContext;
@@ -11,9 +6,13 @@ import org.hibernate.Session;
 public class FacesContextUtil {
     private static final String HIBERNATE_SESSION = "hibernate_session";
     
-    public static void setRequestSession(Session session);
-        FacesContext.GetCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
-    
+    /**
+     *
+     * @param session
+     */
+    public static void setRequestSession(Session session){
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
+    }
     public static Session getRequestSession() {
         return (Session)FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
     }
--- agente_transito/src/main/java/br/com/agente/util/HibernateUtil.java
+++ agente_transito/src/main/java/br/com/agente/util/HibernateUtil.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.agente.util;
 
-//import javax.security.auth.login.Configuration;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-import org.springframework.context.annotation.Configuration;
 
-        
-
-/**
- *
- * @author 36694320191
- */
 public class HibernateUtil {
     private static final SessionFactory sessionfactory;
     public static final String HIBERNATE_SESSION = "hibernate_session"; 
@@ -24,6 +12,7 @@ public class HibernateUtil {
         try {
             System.out.println("tentando abrir uma session factory!");
             Configuration configuration = new Configuration().configure();
+            
             ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
                     applySettings(configuration.getProperties()).buildServiceRegistry();
             
