650bb2f4900901180c2eb912bc6b635645c32a5f
36694320191 <36694320191@A5086.detran.ms.gov.br>
2015-07-23 11:20:29 -0300

    correções nas entidades; criação do managed bean cidade; criação do template; correção do phaseListener

--- agente_transito/src/main/java/br/com/agente/controller/MbCidade.java
+++ agente_transito/src/main/java/br/com/agente/controller/MbCidade.java
@@ -12,18 +12,26 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "mbCidade")
+//suporte a jsf por isso essa anotação @ManagedBean
 @RequestScoped
 public class MbCidade implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+    //instancia de cidade
     private Cidade cidade = new Cidade();
+    //lista de cidades
     private List<Cidade> cidades;
-
+//construtor (alt + insert)
     public MbCidade() {
     }
-
+//criando a interface DAO com a instancia cidadeDAO
     private InterfaceDAO<Cidade> cidadeDAO() {
+        //instanciando o HibernateDAO<Cidade> sendo obrigado a passar via construtor a classe e a sessão do jsf
+        //esse HibernateDAO<Cidade> é genérico, então se precisar criar
+        //cidadeDAO, enderecoDAO, pessoaDAO
+        //fica simples, não preciso implementar um DAO para cada um
+        //basta instanciar passando o nome do DAO, a classe e a sessao do hibernate
+        //já tem um DAO em tempo de execução para todas as minhas entidades.
         InterfaceDAO<Cidade> cidadeDAO = new HibernateDAO<Cidade>(Cidade.class, FacesContextUtil.getRequestSession());
         return cidadeDAO;
     }
@@ -31,6 +39,7 @@ public class MbCidade implements Serializable {
     public String limpCidade() {
         cidade = new Cidade();
         return editCidade();
+        //return "/restrict/cadastrarcidade.faces";
     }
 
     public String editCidade() {
@@ -38,6 +47,8 @@ public class MbCidade implements Serializable {
     }
 
     public String addCidade() {
+        //verifica se o id de cidade está vindo preenchido para adicionar a cidade
+        //senão ele atualiza o registro cidade no banco.
         if (cidade.getIdCidade() == null || cidade.getIdCidade() == 0) {
             insertCidade();
         } else {
@@ -46,9 +57,11 @@ public class MbCidade implements Serializable {
         limpCidade();
         return null;
     }
-
+//reclama que não tem os dois metodos, insertCidade e updateCidade
+//clicar no interrogação e pedir pra criar...
     private void insertCidade() {
         cidadeDAO().save(cidade);
+        //termina de salvar, retorna a mensagem
         FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravação efetuada com sucesso", ""));
     }
--- agente_transito/src/main/webapp/index.jsp
+++ agente_transito/src/main/webapp/index.jsp
@@ -1,21 +1,3 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>DETRAN-MS</title>
-<style type="css">
-    .td{
-    FONT-SIZE: 13;
-    COLOR: #336699;
-    font:"Courier New", Courier, monospace;
-    	}
-</style>
-    </head>
-    <body>
-    
-        <font face="Courier New, Courier, monospace" color="#336699" size="+2">Agente de Trânsito</font>
-    </body>
-</html>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!--quem acessar vai direcionar para a home do jsf-->
+<c:redirect url="/restrict/home.faces"/>
